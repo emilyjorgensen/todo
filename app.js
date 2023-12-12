eac89a2af27e0073fb927d0213a014a7a87bc695
emjorg10 <144714071+emjorg10@users.noreply.github.com>
2023-12-12 14:15:41 -0600

    delete endpoint

--- app.js
+++ app.js
@@ -60,6 +60,25 @@ app.post("/api/v1/todos", (req, res) => {
   });
 });
 
+app.delete("/api/v1/todos/:id", (req, res) => {
+  const id = parseInt(req.params.id, 10);
+
+  db.map((todo, index) => {
+    if (todo.id === id) {
+      db.splice(index, 1);
+      return res.status(200).send({
+        success: "true",
+        message: "Todo deleted successfuly",
+      });
+    }
+  });
+
+  return res.status(404).send({
+    success: "false",
+    message: "todo not found",
+  });
+});
+
 const PORT = 5000;
 
 app.listen(PORT, () => {
